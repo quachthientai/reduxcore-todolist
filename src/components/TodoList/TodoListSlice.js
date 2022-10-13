9f6f3595c1ee86bdcdc24b3bc2fab2cec25c4a17
Tai <quachthientai@gmail.com>
2022-10-13 03:45:23 -0500

    first commit

---
+++ src/components/TodoList/TodoListSlice.js
@@ -0,0 +1,22 @@
+const initState = [
+      { id: 0, name: 'Learn Yoga', completed: false, priority: 'Medium' },
+      { id: 1, name: 'Learn Redux', completed: true, priority: 'High' },
+      { id: 2, name: 'Learn JavaScript', completed: false, priority: 'Low' }
+   ]
+
+
+const todoListReducer = (state = initState, action) => {
+   switch(action.type){
+      case 'todoList/addTodo':
+         return [...state,action.payload]
+      case 'todoList/toggleTodoStatus':
+         return state.map(todo => todo.id === action.payload 
+            ? {...todo, completed: !todo.completed} 
+            : todo 
+      )
+      default:
+         return state
+   }
+}
+
+export default todoListReducer
